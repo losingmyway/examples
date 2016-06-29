ba07bb5a25b7acfbabfce79b38776a350624f186
Clark Nguyen <truong12a62009@gmail.com>
2016-06-29 10:03:37 +0700

    Update bootstrap naviagation menu

--- feature/bootstrap3/js/app2.js
+++ feature/bootstrap3/js/app2.js
@@ -1,5 +1,4 @@
 (function ($) {
-
     'use strict';
 
     // Toggle classes in body for syncing sliding animation with other elements
@@ -17,4 +16,17 @@
             $('body').removeClass('in');
         });
 
+     $('#bs-example-navbar-collapse-3')
+        .on('show.bs.collapse', function (e) {
+            $('body').addClass('menu-slider3');
+        })
+        .on('shown.bs.collapse', function (e) {
+            $('body').addClass('in3');
+        })
+        .on('hide.bs.collapse', function (e) {
+            $('body').removeClass('menu-slider3');
+        })
+        .on('hidden.bs.collapse', function (e) {
+            $('body').removeClass('in3');
+        });
 })(jQuery);
