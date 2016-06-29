d3c95763c56352552dafca8397c463d6033b0f92
Clark Nguyen <truong12a62009@gmail.com>
2016-06-29 17:48:15 +0700

    Update bootstrap

---
+++ feature/bootstrap3/js/app3.js
@@ -0,0 +1,11 @@
+// Dropdown Menu Fade    
+jQuery(document).ready(function(){
+    $(".dropdown").hover(
+        function() { 
+        	$('.dropdown-menu', this).stop().fadeIn("fast");
+    	},
+        function() { 
+        	$('.dropdown-menu', this).stop().fadeOut("fast");
+    	}
+    );
+});
