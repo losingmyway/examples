a1061fb5847fb30a756828ea73cfa7e1b6c5dce2
Clark Nguyen <truong12a62009@gmail.com>
2016-07-24 22:19:50 +0700

    Update js date timezone

--- feature/jquery/js/jquery.removeStyleValue.js
+++ feature/jquery/js/jquery.removeStyleValue.js
@@ -1,18 +1,26 @@
-(function($){
+(function($){ //required
 	var defaultOption = {
 		debug: false,
 		stylekey: ''
 	};
 
+	//removeStyleValue: name of plugin
 	$.fn.removeStyleValue = function(options) {
-		var settings = $.extend(defaultOption, options );
+
+		var settings = $.extend(defaultOption, options ); //extend default value, if options is passed to settings will get values from options
+
+		//add your code process your business
+		
 		if ( !settings.stylekey ) return false;
+
 		var styleStr = $(this).attr('style');
 		if ( defaultOption.debug ) {
 			console.log('styleStr = ' + styleStr);
 		}
+
 		var items = styleStr.split(";");
 		var newResult = '';
+
 		for ( var i = 0; i < items.length; i++ )
 		{
 			if ( items[i] && items[i].indexOf(settings.stylekey) === -1 ) {
@@ -24,6 +32,8 @@
 			console.log('new styleStr = ' + newResult);
 		}
 		$(this).attr('style', newResult);
+
+		return this;
 	};
 
-}(jQuery));
+}(jQuery));//required
